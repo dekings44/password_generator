6fce4f28f59df3cd611fe4b9412300eb836a1800
Kingsley Ihemere <dekings44@gmail.com>
2022-05-23 01:50:16 +0100

    I added a new version of password generator where you only supply how many letters you want in it

---
+++ pass1.py
@@ -0,0 +1,38 @@
+print('Welcome to password generator app')
+
+import random
+
+
+letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 
+'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 
+'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+]
+
+number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',]
+
+symbol = ['!', '#', '$', '£', '@', '%', '&', '(', ')', '*', '+']
+
+
+num_letter = int(input('How many letters would you like in your password?\n'))
+# num_symbol = int(input(f"How many symbols would you like?\n"))
+# numbers = int(input(f"How many numbers would you like?\n"))
+
+
+password = []
+
+for char in range(1, num_letter + 1):
+    password.append(random.choice(letters))
+
+for char in range(1, 3):
+    password.append(random.choice(symbol))
+
+for char in range(1, 5):
+    password.append(random.choice(number))
+
+random.shuffle(password)
+
+
+new_password = ''.join([str(pword) for pword in password])
+
+print(f'Your password is {new_password}')
+    
